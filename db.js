c2724dc1b51d25ede7146ac6d5292a6d327d2e69
Ricky Daly <ricky92d@gmail.com>
2021-11-30 15:57:00 +0000

    mlab added

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/misplaced', { useUnifiedTopology: true, useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/misplaced', { useUnifiedTopology: true, useNewUrlParser: true });
+
+mongoose.connect('mongodb+srv://ricky:a3scpdxa@cluster0.2gwlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })

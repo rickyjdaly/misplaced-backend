45df3c96087fcf073c00a3a46628903331889d15
Ricky Daly <ricky92d@gmail.com>
2021-12-02 15:59:40 +0000

    ssl server added

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const app = require('./app');
 
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
 
 
-app.listen(8080, function(req, res){
-    console.log('Server running...');
+const dirToServe = 'client';
+const port = 3443;
+
+
+// app.listen(8080, function(req, res){
+//     console.log('Server running...');
+// })
+
+
+const httpOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}
+
+https.createServer(httpOptions, app).listen(port, function(){
+    console.log("server running on port 3443...")
 })
-
